2bb1db3a85cfc1e87f2065d95270c9f0b239935a
Camille Avestruz <camille.avestruz@yale.edu>
2017-06-22 17:31:19 -0500

    New ipython notebook with Jens Chluba's SZ map calculation, and using yt to make calculations.  Note: Need python 2.x for this

--- IO/database.py
+++ IO/database.py
@@ -1,9 +1,9 @@
 # Get Database Contents for halo properties (200m and 500c)
 import L500analysis.caps.io.reader as db
 
-sim = db.Simulation('L500_NR_0',db_dir='/home/babyostrich/Documents/Repos/L500analysis/simulation_databases/'
+sim = db.Simulation('L500_NR_0',db_dir='/home/babyostrich/data/databases/'
                   )
 
 def collect_halo_props(halo_ids, props=['r200m','r500c','M_total_200m','M_total_500c'], aexp='1.0005') :
     
-    return halo_props = sim.get_halo_properties(halo_ids, props, aexp)
+    return sim.get_halo_properties(halo_ids, props, aexp)
