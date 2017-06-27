4e9625a886c6db7432abb86d4586a7c05d50fb68
Camille Avestruz <camille.avestruz@yale.edu>
2017-06-27 13:02:32 -0500

    added script to calculate sz masses

---
+++ calculate_scaling/print_sz_mass.py
@@ -0,0 +1,94 @@
+''' Prints out the sz scaling '''
+
+import L500analysis.caps.io.reader as db
+from yt.units import kpc, Msun, Mpc
+from SZmaps.IO import get_fits_data as gf
+
+database_dir = '/home/babyostrich/data/databases/'
+database_sim_name = 'L500_NR_0'
+
+fits_dir = '/home/babyostrich/data/L500_fits/'
+aexp = '1.0005'
+
+# Load the database and get properties of interest
+sim = db.Simulation(database_sim_name,
+                       db_dir=database_dir
+                                         )
+#halo_ids = sim.get_halo_ids()
+halo_ids = [1]
+props =['r200m','r500c','M_total_200m','M_total_500c']
+
+halo_props = sim.get_halo_properties(halo_ids, props, aexp)
+
+def integrate_in_yt_volume(yt_volume_type, field_name, *volume_type_params) :
+
+    yt_volume = yt_volume_type(*volume_type_params)
+    integrated_szy = yt_volume[field_name].sum().in_units('1/Mpc')
+    volume = yt_volume.volume().in_units('Mpc**3')
+    
+    return integrated_szy * volume
+
+
+from collections import defaultdict
+
+szy_scaling = defaultdict(list)
+
+for halo_id in halo_ids :
+    
+    szy_scaling['halo_ids'].append( halo_id )
+
+    # Create yt datastructure
+    yt_datastruct = gf.create_yt_datastruc_from_cubes(fits_dir,aexp,halo_id, 6)
+    dd = yt_datastruct.all_data()
+
+    # Get the halo properties from the database
+    halo_prop = halo_props.loc[halo_props['id'] == halo_id]
+    r200m = halo_prop['r200m'][0] #* kpc 
+    r500c = halo_prop['r500c'][0] #* kpc
+
+    M200m = halo_prop['M_total_200m'][0] #* Msun
+    M500c = halo_prop['M_total_500c'][0] #* Msun
+
+    szy_scaling['r200m'].append(r200m)
+    szy_scaling['r500c'].append(r500c)
+    szy_scaling['M500c'].append(M500c)
+    szy_scaling['M200m'].append(M200m)
+    
+    # Get the angular diameter distance with minimum observable redshift to 0.02
+    from yt.utilities.cosmology import Cosmology
+    cosmo = Cosmology()
+    (zi, zf) = ( 0,  max(0.02,1./float(aexp)-1.) )
+    dA = cosmo.angular_diameter_distance(zi,zf).in_units('Mpc')
+    
+    # Integrate in a sphere
+    center = [0.5, 0.5, 0.5]
+    Ysz500c = integrate_in_yt_volume( yt_datastruct.sphere, 'szy',
+                                 center,(r500c,'kpc') ) / dA**2
+
+    szy_scaling['Ysz500c'].append(Ysz500c.value)
+    # Integrate in a cylinder
+    radius = ( r500c, 'kpc' )
+    depth = ( 3*r500c, 'kpc' )
+    normal_vectors = {'x': [1,0,0],
+                      'y': [0,1,0],
+                      'z': [0,0,1]
+    }
+
+    
+    for los, normal_vector in normal_vectors.iteritems() :
+        Yszcylinder = integrate_in_yt_volume( yt_datastruct.disk, 'szy',
+                                              center, normal_vector,
+                                              radius, depth ) / dA**2
+        szy_scaling['Yszcyl_'+los].append( Yszcylinder.value )
+
+        
+import pandas as pd
+
+df = pd.DataFrame(szy_scaling)
+df.to_csv( path_or_buf='szyscaling', sep=' ', index=False )
+
+
+
+
+    
+        
