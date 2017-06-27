01a48ff5da8858e81ad0d05a6da2438693836362
Camille Avestruz <camille.avestruz@yale.edu>
2017-06-27 13:09:51 -0500

    adding modifications

--- IO/database.py
+++ IO/database.py
@@ -3,7 +3,8 @@ import L500analysis.caps.io.reader as db
 
 sim = db.Simulation('L500_NR_0',db_dir='/home/babyostrich/data/databases/'
                   )
+halo_ids = sim.get_halo_ids()
 
-def collect_halo_props(halo_ids, props=['r200m','r500c','M_total_200m','M_total_500c'], aexp='1.0005') :
+def collect_halo_props(halo_ids=halo_ids, props=['r200m','r500c','M_total_200m','M_total_500c'], aexp='1.0005') :
     
     return sim.get_halo_properties(halo_ids, props, aexp)
--- IO/get_fits_data.py
+++ IO/get_fits_data.py
@@ -1,5 +1,6 @@
 '''Main method to generate a yt data structure from fits cubes'''
-from _tools import ytname2artname, collect_art_data_arrays
+from _tools import ytname2artname, \
+        collect_art_data_arrays, calculate_length_unit_box
 import numpy as np
 
 def create_yt_datastruc_from_cubes(fitsdir, aexp, halo_id, Lv,
@@ -19,7 +20,7 @@ def create_yt_datastruc_from_cubes(fitsdir, aexp, halo_id, Lv,
             data[yt_field] = (art_data_array, art['units'])
             
         length_unit = calculate_length_unit_box( Lbox_fits, max_Lv_kpc, Lv )
-        
+        import yt
         return yt.load_uniform_grid(data, Lbox_fits, length_unit=length_unit)
         
 
